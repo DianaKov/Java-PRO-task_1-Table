ff568848c36b224338c9e0b8b90c60ba1ed7f2d9
Diana Kovalevska <119618395+DianaKov@users.noreply.github.com>
2023-02-20 20:54:18 +0200

    Update AnnotationStatistics.java

--- src/main/java/com/annotation/AnnotationStatistics.java
+++ src/main/java/com/annotation/AnnotationStatistics.java
@@ -5,6 +5,22 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 
 public class AnnotationStatistics {
+    
+    public static <T> int countAnnotatedMethods(T firstClass, Class<? extends Annotation> firstAnnotationClass) {
+        int count = 0;
+        Class<?> cls = firstClass.getClass();
+        while (!cls.equals(Object.class)) {
+            for (Method method : cls.getDeclaredMethods()) {
+                if (method.isAnnotationPresent(firstAnnotationClass)) {
+                    count++;
+                }
+            }
+            cls = cls.getSuperclass();
+        }
+        System.out.println(firstAnnotationClass + " has been used " + count + " times");
+        return count;
+    }
+
     public static <T> int countAnnotatedParameter(T firstClass, Class<? extends Annotation> secondAnnotationClass) {
         int count = 0;
         Class<?> cls = firstClass.getClass();
@@ -19,21 +35,7 @@ public class AnnotationStatistics {
                 }
             }
         }
-        System.out.println("Number of parameter annotated with @SecondAnnotation: " + count);
-        return count;
-    }
-    public static <T> int countAnnotatedMethods(T firstClass, Class<? extends Annotation> firstAnnotationClass) {
-        int count = 0;
-        Class<?> cls = firstClass.getClass();
-        while (!cls.equals(Object.class)) {
-            for (Method method : cls.getDeclaredMethods()) {
-                if (method.isAnnotationPresent(firstAnnotationClass)) {
-                    count++;
-                }
-            }
-            cls = cls.getSuperclass();
-        }
-        System.out.println("Number of methods annotated with @FirstAnnotation: " + count);
+        System.out.println(secondAnnotationClass + " has been used " + count + " times");
         return count;
     }
 }
