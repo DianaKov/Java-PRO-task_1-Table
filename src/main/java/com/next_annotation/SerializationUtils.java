cb994e5512abd312849d8c52515c6865d2c6adfb
Diana Kovalevska <119618395+DianaKov@users.noreply.github.com>
2023-02-26 21:24:22 +0200

    Update SerializationUtils.java

--- src/main/java/com/next_annotation/SerializationUtils.java
+++ src/main/java/com/next_annotation/SerializationUtils.java
@@ -4,21 +4,14 @@ import java.io.*;
 import java.lang.reflect.Field;
 
 public class SerializationUtils {
-    /**
-     * Serialize an object to a file
-     *
-     * @param obj the object to serialize
-     * @param filename the name of the file to save the serialized object to
-     */
+
     public static void serialize(Object obj, String filename) {
         try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filename))) {
-            // Get all fields of the object's class
             Field[] fields = obj.getClass().getDeclaredFields();
             for (Field field : fields) {
-                // Check if the field has the @Save annotation
                 if (field.isAnnotationPresent(Save.class)) {
-                    field.setAccessible(true); // Make the field accessible (even if it's private)
-                    out.writeObject(field.get(obj)); // Serialize the field value
+                    field.setAccessible(true); 
+                    out.writeObject(field.get(obj)); 
                 }
             }
         } catch (IOException | IllegalAccessException e) {
@@ -26,25 +19,16 @@ public class SerializationUtils {
         }
     }
 
-    /**
-     * Deserialize an object from a file
-     *
-     * @param cls the class of the object to deserialize
-     * @param filename the name of the file to deserialize the object from
-     * @return the deserialized object
-     */
     public static Object deserialize(Class<?> cls, String filename) {
         Object obj = null;
         try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename))) {
-            obj = cls.newInstance(); // Create a new instance of the class to deserialize into
-            // Get all fields of the object's class
+            obj = cls.newInstance(); 
             Field[] fields = cls.getDeclaredFields();
             for (Field field : fields) {
-                // Check if the field has the @Save annotation
                 if (field.isAnnotationPresent(Save.class)) {
-                    field.setAccessible(true); // Make the field accessible (even if it's private)
-                    Object fieldValue = in.readObject(); // Deserialize the field value
-                    field.set(obj, fieldValue); // Set the field value in the object
+                    field.setAccessible(true); 
+                    Object fieldValue = in.readObject();
+                    field.set(obj, fieldValue); 
                 }
             }
         } catch (IOException | IllegalAccessException | InstantiationException | ClassNotFoundException e) {
