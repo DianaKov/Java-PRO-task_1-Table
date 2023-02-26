5fa2656dc09c2112d908ddb16b8d0c9345e843c8
Diana Kovalevska <119618395+DianaKov@users.noreply.github.com>
2023-02-26 21:26:38 +0200

    Add files via upload

---
+++ src/main/java/com/next_annotation/Main.java
@@ -0,0 +1,28 @@
+package com.next_annotation;
+
+public class Main {
+    @Save
+    private String myString;
+    @Save
+    private int myInt;
+    private double myDouble;
+
+    public Main() {
+
+    }
+    public Main(String myString, int myInt, double myDouble) {
+        this.myString = myString;
+        this.myInt = myInt;
+        this.myDouble = myDouble;
+    }
+
+    public static void main(String[] args) {
+        Main obj = new Main("Java", 2023, 3.14);
+        SerializationUtils.serialize(obj, "myclass.ser");
+        Main obj2 = (Main) SerializationUtils.deserialize(Main.class, "myclass.ser");
+        System.out.println(obj2.myString); // Output: Hello
+        System.out.println(obj2.myInt); // Output: 123
+        System.out.println(obj2.myDouble); // Output: 0.0 (not annotated with @Save)
+    }
+}
+
