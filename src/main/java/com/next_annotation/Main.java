f6f3f880d12628f0d8a0763363e113d8552d3807
Diana Kovalevska <119618395+DianaKov@users.noreply.github.com>
2023-03-04 11:35:48 +0200

    Update Main.java

--- src/main/java/com/next_annotation/Main.java
+++ src/main/java/com/next_annotation/Main.java
@@ -1,25 +1,32 @@
 package com.next_annotation;
 
+import java.io.IOException;
+
 public class Main {
     @Save
-    private String myString;
+    public String myString;
     @Save
-    private int myInt;
-    private double myDouble;
+    public int myInt;
+    public double myDouble;
 
-    public Main() {
-
-    }
     public Main(String myString, int myInt, double myDouble) {
         this.myString = myString;
         this.myInt = myInt;
         this.myDouble = myDouble;
     }
 
-    public static void main(String[] args) {
+    public Main() {
+
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, IllegalAccessException {
         Main obj = new Main("Java", 2023, 3.14);
-        SerializationUtils.serialize(obj, "myclass.ser");
-        Main obj2 = (Main) SerializationUtils.deserialize(Main.class, "myclass.ser");
+        SerializeUtil.serialize(obj, "obj.txt");
+        System.out.println(obj.myString); // Output: Java
+        System.out.println(obj.myInt); // Output: 2023
+        System.out.println(obj.myDouble); // Output: 3.14
+        Main obj2 = new Main();
+        SerializeUtil.deserialize(obj2, "obj.txt");
         System.out.println(obj2.myString); // Output: Java
         System.out.println(obj2.myInt); // Output: 2023
         System.out.println(obj2.myDouble); // Output: 0.0 (not annotated with @Save)
